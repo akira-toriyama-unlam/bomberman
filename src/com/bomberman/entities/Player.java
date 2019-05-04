abdb3a7856968bfcd01c69151483736b05941675
Ariel Gonzalez <ariel.damian.gonzalez@gmail.com>
2019-05-04 02:43:27 -0300

    Add multiple-bombs functionality.

--- src/com/bomberman/entities/Player.java
+++ src/com/bomberman/entities/Player.java
@@ -1,40 +1,41 @@
 package com.bomberman.entities;
-import com.bomberman.entities.Direction;
 
 public class Player extends Entity implements ExplosionListener {
 
+	private static final int CONCURRENT_BOMBS = 1;
+	private int bombsCount;
 	private boolean alive;
 
 	public Player(int x, int y, GameMap map) {
 		super(x, y, true, map, true);
 		this.alive = true;
+		this.bombsCount = Player.CONCURRENT_BOMBS;
 	}
 
 	public void placeBomb(GameMap gameMap) {
-		Bomb bomb = new Bomb(x, y, gameMap,this);
-		gameMap.addObject(bomb);
+		if (this.bombsCount > 0) {
+			Bomb bomb = new Bomb(x, y, gameMap, this);
+			gameMap.addObject(bomb);
+			bombsCount--;
+		}
 	}
 
 	public void move(Direction direction) {
-		
 		if (this.map.canMove(this.x, this.y, direction)) {
-//			// TODO add logic to move
-	
-			switch (direction) { 
-				case UP:
-					this.y++;
-					break;
-				case DOWN:
-					this.y--;
-					break;
-				case LEFT:
-					this.x--;
-					break;
-				case RIGHT:
-					this.x++;
-					break;
+			switch (direction) {
+			case UP:
+				this.y++;
+				break;
+			case DOWN:
+				this.y--;
+				break;
+			case LEFT:
+				this.x--;
+				break;
+			case RIGHT:
+				this.x++;
+				break;
 			}
-//			
 		}
 	}
 
@@ -47,12 +48,10 @@ public class Player extends Entity implements ExplosionListener {
 	public boolean isAlive() {
 		return alive;
 	}
-	
-
 
 	@Override
 	public void update() {
-		// Bomb has no longer exists, this Player can use a new bomb.
+		bombsCount++;
 	}
 
 }
--- tests/com/bomberman/tests/PlayerTest.java
+++ tests/com/bomberman/tests/PlayerTest.java
@@ -1,6 +1,7 @@
 package com.bomberman.tests;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.bomberman.entities.Direction;
@@ -10,154 +11,163 @@ import com.bomberman.entities.Tile;
 
 public class PlayerTest {
 
+	GameMap m;
+	Player p;
+
+	@Before
+	public void beforeFunc() {
+		m = new GameMap("mapTest", 50, 50);
+	}
+
 	@Test
 	public void moveUpWithOutObj() {
-		GameMap m = new GameMap("mapTest", 50, 50);
-		Player p = new Player(0, 0, m);
-		
+		p = new Player(0, 0, m);
+
 		p.move(Direction.UP);
-		
-		Assert.assertEquals(p.getY(),1);
-		Assert.assertEquals(p.getX(),0);
-		
+
+		Assert.assertEquals(p.getY(), 1);
+		Assert.assertEquals(p.getX(), 0);
+
 		p.move(Direction.UP);
-		
-		Assert.assertEquals(p.getY(),2);
-		Assert.assertEquals(p.getX(),0);
+
+		Assert.assertEquals(p.getY(), 2);
+		Assert.assertEquals(p.getX(), 0);
 	}
-	
+
 	@Test
 	public void moveLeftWithOutObj() {
-		GameMap m = new GameMap("mapTest", 50, 50);
-		Player p = new Player(1, 0, m);
-		
+		p = new Player(1, 0, m);
+
 		p.move(Direction.LEFT);
-		Assert.assertEquals(p.getX(),0);
-		Assert.assertEquals(p.getY(),0);
-		
+		Assert.assertEquals(p.getX(), 0);
+		Assert.assertEquals(p.getY(), 0);
+
 	}
-	
+
 	@Test
 	public void moveDownWithOutObj() {
-		
-		GameMap m = new GameMap("mapTest", 50, 50);
-		Player p = new Player(0, 1, m);
-		
+		p = new Player(0, 1, m);
+
 		p.move(Direction.DOWN);
-		Assert.assertEquals(p.getY(),0);
-		Assert.assertEquals(p.getX(),0);
-		
+		Assert.assertEquals(p.getY(), 0);
+		Assert.assertEquals(p.getX(), 0);
+
 	}
-	
+
 	@Test
 	public void moveRightWithOutObj() {
-		
-		GameMap m = new GameMap("mapTest", 50, 50);
-		Player p = new Player(0, 0, m);
-		
+		p = new Player(0, 0, m);
+
 		p.move(Direction.RIGHT);
-		Assert.assertEquals(p.getY(),0);
-		Assert.assertEquals(p.getX(),1);
+		Assert.assertEquals(p.getY(), 0);
+		Assert.assertEquals(p.getX(), 1);
 		p.move(Direction.RIGHT);
-		Assert.assertEquals(p.getY(),0);
-		Assert.assertEquals(p.getX(),2);
+		Assert.assertEquals(p.getY(), 0);
+		Assert.assertEquals(p.getX(), 2);
 	}
-	
+
 	@Test
 	public void moveUpWithObj() {
-			
-		GameMap m = new GameMap("mapTest", 50, 50);
-		Player p = new Player(0, 0, m);
-		Tile t = new Tile(0,1,false,m,false);
+		p = new Player(0, 0, m);
+		Tile t = new Tile(0, 1, false, m, false);
 		m.addObject(t);
 		p.move(Direction.UP);
-		
-		Assert.assertEquals(p.getY(),0);
-		Assert.assertEquals(p.getX(),0);
-		
+
+		Assert.assertEquals(p.getY(), 0);
+		Assert.assertEquals(p.getX(), 0);
+
 	}
-	
+
 	@Test
 	public void moveDownWithObjOver() {
-			
-		GameMap m = new GameMap("mapTest", 50, 50);
-		Player p = new Player(0, 1, m);
-		Tile t = new Tile(0,-1,false,m, true);
+		p = new Player(0, 1, m);
+		Tile t = new Tile(0, -1, false, m, true);
 		m.addObject(t);
-		
+
 		p.move(Direction.DOWN);
-		
-		Assert.assertEquals(p.getY(),0);
-		Assert.assertEquals(p.getX(),0);
-		
+
+		Assert.assertEquals(p.getY(), 0);
+		Assert.assertEquals(p.getX(), 0);
+
 	}
-	
+
 	@Test
-	public void placeBombAndWalkOver(){
-		GameMap m = new GameMap("mapTest", 50, 50);
-		Player p = new Player(1, 1, m);
+	public void placeBombAndWalkOver() {
+		p = new Player(1, 1, m);
 		p.placeBomb(m);
-		
+
 		p.move(Direction.DOWN);
-		Assert.assertEquals(p.getY(),0);
-		
+		Assert.assertEquals(p.getY(), 0);
+
 		p.move(Direction.UP);
-		Assert.assertEquals(p.getY(),1);
-		
+		Assert.assertEquals(p.getY(), 1);
+
 		p.move(Direction.UP);
-		Assert.assertEquals(p.getY(),2);
-		
+		Assert.assertEquals(p.getY(), 2);
+
 		p.move(Direction.DOWN);
-		Assert.assertEquals(p.getY(),1);
-		
+		Assert.assertEquals(p.getY(), 1);
+
 		p.move(Direction.RIGHT);
-		Assert.assertEquals(p.getX(),2);
-		
+		Assert.assertEquals(p.getX(), 2);
+
 		p.move(Direction.LEFT);
-		Assert.assertEquals(p.getX(),1);
-		
+		Assert.assertEquals(p.getX(), 1);
+
 		p.move(Direction.LEFT);
-		Assert.assertEquals(p.getX(),0);
+		Assert.assertEquals(p.getX(), 0);
 	}
-	
+
 	@Test
 	public void cantMoveLeftOutOfMap() {
-		GameMap m = new GameMap("mapTest", 50, 50);
-		Player p = new Player(0, 0, m);
-		
+		p = new Player(0, 0, m);
+
 		p.move(Direction.LEFT);
-		Assert.assertEquals(p.getX(),0);
-		Assert.assertEquals(p.getY(),0);
+		Assert.assertEquals(p.getX(), 0);
+		Assert.assertEquals(p.getY(), 0);
 	}
-	
+
 	@Test
 	public void cantMoveDownOutOfMap() {
-		GameMap m = new GameMap("mapTest", 50, 50);
-		Player p = new Player(0, 0, m);
-		
+		p = new Player(0, 0, m);
+
 		p.move(Direction.DOWN);
-		Assert.assertEquals(p.getX(),0);
-		Assert.assertEquals(p.getY(),0);
+		Assert.assertEquals(p.getX(), 0);
+		Assert.assertEquals(p.getY(), 0);
 	}
-	
+
 	@Test
 	public void cantMoveRightOutOfMap() {
-		GameMap m = new GameMap("mapTest", 50, 50);
-		Player p = new Player(50, 50, m);
-		
+		p = new Player(50, 50, m);
+
 		p.move(Direction.RIGHT);
-		Assert.assertEquals(p.getX(),50);
-		Assert.assertEquals(p.getY(),50);
+		Assert.assertEquals(p.getX(), 50);
+		Assert.assertEquals(p.getY(), 50);
 	}
-	
+
 	@Test
 	public void cantMoveUptOutOfMap() {
-		GameMap m = new GameMap("mapTest", 50, 50);
-		Player p = new Player(50, 50, m);
-		
+		p = new Player(50, 50, m);
+
 		p.move(Direction.UP);
-		Assert.assertEquals(p.getX(),50);
-		Assert.assertEquals(p.getY(),50);
+		Assert.assertEquals(p.getX(), 50);
+		Assert.assertEquals(p.getY(), 50);
 	}
-	
+
+	@Test
+	public void shouldCanPlaceOneBombAtTheTime() {
+		p = new Player(50, 50, m);
+		p.placeBomb(m);
+		p.placeBomb(m);
+		Assert.assertEquals(1, m.getObjects().size());
+	}
+
+	@Test
+	public void shouldCanPlaceAnotherBombAfterWaitForExplosion() throws InterruptedException {
+		p = new Player(50, 50, m);
+		p.placeBomb(m);
+		Thread.sleep(2200);
+		p.placeBomb(m);
+		Assert.assertEquals(1, m.getObjects().size());
+	}
 }
