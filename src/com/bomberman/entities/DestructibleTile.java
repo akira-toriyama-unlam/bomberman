04ca64ada0640e458927769a0174942b6510f189
Ariel Gonzalez <ariel.damian.gonzalez@gmail.com>
2019-05-31 23:58:54 -0300

    Create DestructibleTile.

---
+++ src/com/bomberman/entities/DestructibleTile.java
@@ -0,0 +1,10 @@
+package com.bomberman.entities;
+
+public class DestructibleTile extends Tile implements Destructible {
+
+	public DestructibleTile(int x, int y, GameMap map, boolean canOver) {
+		super(x, y, map, canOver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
