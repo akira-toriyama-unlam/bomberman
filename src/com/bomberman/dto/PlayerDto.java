6f409083df3cb70cdcb5c7606ab2fe9261e8e223
Alejandro Imperatori <alejandroimperatori@gmail.com>
2019-07-12 20:01:49 -0300

    fixed player dead

--- src/com/bomberman/dto/PlayerDto.java
+++ src/com/bomberman/dto/PlayerDto.java
@@ -7,7 +7,7 @@ import com.bomberman.graphics.PlayerSpriteManager;
 import com.bomberman.graphics.Sprite;
 
 public class PlayerDto {
-	
+
 	private double x;
 	private double y;
 	private Integer id;
@@ -15,7 +15,8 @@ public class PlayerDto {
 	private boolean destroyed;
 	private boolean painted;
 
-	public PlayerDto(double x, double y, Integer id, MovementStatusDto movementStatusDto, boolean destroyed, boolean painted) {
+	public PlayerDto(double x, double y, Integer id, MovementStatusDto movementStatusDto, boolean destroyed,
+			boolean painted) {
 		this.id = id;
 		this.x = x;
 		this.y = y;
@@ -39,27 +40,27 @@ public class PlayerDto {
 	public void setY(double y) {
 		this.y = y;
 	}
-	
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
 	public Integer getId() {
 		return this.id;
 	}
-	
+
 	public boolean isDestroyed() {
 		return this.destroyed;
 	}
-	
+
 	public void setDestroyed(boolean destroyed) {
 		this.destroyed = destroyed;
 	}
-	
+
 	public boolean isPainted() {
 		return this.painted;
 	}
-	
+
 	public MovementStatusDto getMovementStatusDto() {
 		return movementStatusDto;
 	}
@@ -71,35 +72,51 @@ public class PlayerDto {
 	public Player toPlayer() {
 		return new Player(x, y, id);
 	}
-	
+
 	public Image chooseSprite() {
-		switch(this.movementStatusDto.getDirection()) {
-		case UP:
-			if(this.movementStatusDto.isMoving()) {
-				return Sprite.movingSprite(PlayerSpriteManager.getInstance(id).getPlayerSpriteUp(0), PlayerSpriteManager.getInstance(id).getPlayerSpriteUp(1),
-						PlayerSpriteManager.getInstance(id).getPlayerSpriteUp(2), this.movementStatusDto.getAnimateCount(), 5);
+		if (!painted) {
+			switch (this.movementStatusDto.getDirection()) {
+			case UP:
+				if (this.movementStatusDto.isMoving()) {
+					return Sprite.movingSprite(PlayerSpriteManager.getInstance(id).getPlayerSpriteUp(0),
+							PlayerSpriteManager.getInstance(id).getPlayerSpriteUp(1),
+							PlayerSpriteManager.getInstance(id).getPlayerSpriteUp(2),
+							this.movementStatusDto.getAnimateCount(), 5);
+				}
+				return PlayerSpriteManager.getInstance(id).getPlayerSpriteUp(0);
+			case RIGHT:
+				if (this.movementStatusDto.isMoving()) {
+					return Sprite.movingSprite(PlayerSpriteManager.getInstance(id).getPlayerSpriteRight(0),
+							PlayerSpriteManager.getInstance(id).getPlayerSpriteRight(1),
+							PlayerSpriteManager.getInstance(id).getPlayerSpriteRight(0),
+							this.movementStatusDto.getAnimateCount(), 5);
+				}
+				return PlayerSpriteManager.getInstance(id).getPlayerSpriteRight(0);
+			case DOWN:
+				if (this.movementStatusDto.isMoving()) {
+					return Sprite.movingSprite(PlayerSpriteManager.getInstance(id).getPlayerSpriteDown(0),
+							PlayerSpriteManager.getInstance(id).getPlayerSpriteDown(1),
+							PlayerSpriteManager.getInstance(id).getPlayerSpriteDown(2),
+							this.movementStatusDto.getAnimateCount(), 5);
+				}
+				return PlayerSpriteManager.getInstance(id).getPlayerSpriteDown(0);
+			case LEFT:
+				if (this.movementStatusDto.isMoving()) {
+					return Sprite.movingSprite(PlayerSpriteManager.getInstance(id).getPlayerSpriteLeft(0),
+							PlayerSpriteManager.getInstance(id).getPlayerSpriteLeft(1),
+							PlayerSpriteManager.getInstance(id).getPlayerSpriteLeft(2),
+							this.movementStatusDto.getAnimateCount(), 5);
+				}
+				return PlayerSpriteManager.getInstance(id).getPlayerSpriteLeft(0);
+			default:
+				return null;
 			}
-			return PlayerSpriteManager.getInstance(id).getPlayerSpriteUp(0);
-		case RIGHT:
-			if(this.movementStatusDto.isMoving()) {
-				return Sprite.movingSprite(PlayerSpriteManager.getInstance(id).getPlayerSpriteRight(0), PlayerSpriteManager.getInstance(id).getPlayerSpriteRight(1),
-						PlayerSpriteManager.getInstance(id).getPlayerSpriteRight(0), this.movementStatusDto.getAnimateCount(), 5);
-			}
-			return PlayerSpriteManager.getInstance(id).getPlayerSpriteRight(0);
-		case DOWN:
-			if(this.movementStatusDto.isMoving()) {
-				return Sprite.movingSprite(PlayerSpriteManager.getInstance(id).getPlayerSpriteDown(0), PlayerSpriteManager.getInstance(id).getPlayerSpriteDown(1),
-						PlayerSpriteManager.getInstance(id).getPlayerSpriteDown(2), this.movementStatusDto.getAnimateCount(), 5);
-			}
-			return PlayerSpriteManager.getInstance(id).getPlayerSpriteDown(0);
-		case LEFT:
-			if(this.movementStatusDto.isMoving()) {
-				return Sprite.movingSprite(PlayerSpriteManager.getInstance(id).getPlayerSpriteLeft(0), PlayerSpriteManager.getInstance(id).getPlayerSpriteLeft(1),
-						PlayerSpriteManager.getInstance(id).getPlayerSpriteLeft(2), this.movementStatusDto.getAnimateCount(), 5);
-			}
-			return PlayerSpriteManager.getInstance(id).getPlayerSpriteLeft(0);
-		default:
-			return null;
+		} else {
+			return Sprite.movingSprite(PlayerSpriteManager.getInstance(id).getPlayerSpriteDead(0),
+					PlayerSpriteManager.getInstance(id).getPlayerSpriteDead(1),
+					PlayerSpriteManager.getInstance(id).getPlayerSpriteDead(2),
+					this.movementStatusDto.getAnimateCount(), 5);
 		}
+
 	}
 }
